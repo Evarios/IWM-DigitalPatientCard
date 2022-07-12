c963dfa2459821578626a9b2d35480702cd56d06
Evarios <jan.brozdziak@gmail.com>
2022-07-12 18:40:01 +0200

    added observation list to patient data

--- DigitalPatientCard.py
+++ DigitalPatientCard.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request
 from fhirpy import SyncFHIRClient
+from datetime import datetime
+from dateutil import parser
 
 # setup
 app = Flask(__name__)
@@ -42,14 +45,36 @@ def patients_page():
 
 @app.route("/patient/<id>")
 def patient_page(id):
-    pt = client.reference('Patient', id).to_resource()
+    patient_reference = client.reference('Patient', id)
+    patient_resource = patient_reference.to_resource()
+    patient_birthdate_string =  patient_resource['birthDate']
     patient = {
-            'name': pt['name'][0].given[0],
-            'surname': pt['name'][0].family,
-            'id': pt['id']
+            'name': patient_resource['name'][0].given[0],
+            'surname': patient_resource['name'][0].family,
+            'id': patient_resource['id'],
+            'gender': patient_resource['gender'],
+            'birthDate': parser.parse(patient_birthdate_string)
     }
-    return render_template("patientData.html", patient=patient)
+    
+    observations = client.resources('Observation').search(subject=patient_reference)
+    observation_list = []
+    for o in observations:
+        observation = {}
+        observation['name'] = o.to_resource()['code']['coding'][0]['display']
+        observation_date_string = o.to_resource()['issued']
+        observation['date'] = parser.parse(observation_date_string)
+        if 'valueQuantity' in o:
+            observation['value'] = o['valueQuantity']['value']
+            observation['unit'] = o['valueQuantity']['unit']
+        observation_list.append(observation)
+    
+    observation_list = sorted(observation_list, key= lambda d: d['date'], reverse=True)
+
+    return render_template("patientData.html", patient_data=patient, medical_data=observation_list)
     
 
 if __name__ == "__main__":
+    patient = client.reference('Patient', '31191928-6acb-4d73-931c-e601cc3a13fa')
+    observations = client.resources('Observation').search(subject=patient)
+    print(patient.to_resource()['gender'])
     app.run(debug=True)
