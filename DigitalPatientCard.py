218d1a22854ec55fe14e03cda06c236368299016
Evarios <jan.brozdziak@gmail.com>
2022-09-26 12:23:59 +0200

    todo medication request

--- DigitalPatientCard.py
+++ DigitalPatientCard.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request
 from fhirpy import SyncFHIRClient
-from datetime import datetime
 from dateutil import parser
+import pytz
 
 # setup
 app = Flask(__name__)
@@ -11,7 +10,7 @@ client = SyncFHIRClient("http://localhost:8080/baseR4")
 
 @app.route("/")
 def home():
-    return redirect(url_for("patients_page")) 
+    return redirect(url_for("patients_page"))
 
 
 @app.route("/patientsList", methods=['POST', 'GET'])
@@ -43,38 +42,100 @@ def patients_page():
         return render_template("patientsList.html", patients=patients_list)
 
 
-@app.route("/patient/<id>")
+@app.route("/patient/<id>", methods=['POST', 'GET'])
 def patient_page(id):
-    patient_reference = client.reference('Patient', id)
-    patient_resource = patient_reference.to_resource()
-    patient_birthdate_string =  patient_resource['birthDate']
-    patient = {
+    if request.method == 'GET':
+        patient_reference = client.reference('Patient', id)
+        patient_resource = patient_reference.to_resource()
+        patient_birthdate_string = patient_resource['birthDate']
+        patient = {
             'name': patient_resource['name'][0].given[0],
             'surname': patient_resource['name'][0].family,
             'id': patient_resource['id'],
             'gender': patient_resource['gender'],
-            'birthDate': parser.parse(patient_birthdate_string)
-    }
-    
-    observations = client.resources('Observation').search(subject=patient_reference)
-    observation_list = []
-    for o in observations:
-        observation = {}
-        observation['name'] = o.to_resource()['code']['coding'][0]['display']
-        observation_date_string = o.to_resource()['issued']
-        observation['date'] = parser.parse(observation_date_string)
-        if 'valueQuantity' in o:
-            observation['value'] = o['valueQuantity']['value']
-            observation['unit'] = o['valueQuantity']['unit']
-        observation_list.append(observation)
-    
-    observation_list = sorted(observation_list, key= lambda d: d['date'], reverse=True)
+            'birthDate': parser.parse(patient_birthdate_string).strftime("%m/%d/%Y")
+        }
+        
+        #Get observations
+        observations = client.resources(
+            'Observation').search(subject=patient_reference)
+        observation_list = []
+        for o in observations:
+            observation = {}
+            observation['name'] = o.to_resource(
+            )['code']['coding'][0]['display']
+            observation_date_string = o.to_resource()['issued']
+            observation['date'] = parser.parse(
+                observation_date_string).strftime("%m/%d/%Y, %H:%M:%S")
+            observation['dateSort'] = parser.parse(observation_date_string)
+            if 'valueQuantity' in o:
+                observation['value'] = round(o['valueQuantity']['value'], 2)
+                observation['unit'] = o['valueQuantity']['unit']
+            observation_list.append(observation)
 
-    return render_template("patientData.html", patient_data=patient, medical_data=observation_list)
-    
+        observation_list = sorted(
+            observation_list, key=lambda d: d['dateSort'], reverse=True)
+        
+        #TODO get and display medications
+
+        return render_template("patientData.html", patient_data=patient, medical_data=observation_list)
+    else:
+
+        date_from = request.form['dateFrom']
+        date_to = request.form['dateTo']
+        if date_from != "":
+            datetime_from = parser.parse(date_from).replace(tzinfo=pytz.utc)
+        if date_to != "":
+            datetime_to = parser.parse(date_to).replace(tzinfo=pytz.utc)
+
+        patient_reference = client.reference('Patient', id)
+        patient_resource = patient_reference.to_resource()
+        patient_birthdate_string = patient_resource['birthDate']
+        patient = {
+            'name': patient_resource['name'][0].given[0],
+            'surname': patient_resource['name'][0].family,
+            'id': patient_resource['id'],
+            'gender': patient_resource['gender'],
+            'birthDate': parser.parse(patient_birthdate_string).strftime("%m/%d/%Y")
+        }
+        
+        #get observations
+        observations = client.resources(
+            'Observation').search(subject=patient_reference)
+        observation_list = []
+        for o in observations:
+            observation = {}
+            observation['name'] = o.to_resource(
+            )['code']['coding'][0]['display']
+            observation_date_string = o.to_resource()['issued']
+            observation['date'] = parser.parse(
+                observation_date_string).strftime("%m/%d/%Y, %H:%M:%S")
+            observation['dateSort'] = parser.parse(observation_date_string).replace(tzinfo=pytz.utc)
+            if 'valueQuantity' in o:
+                observation['value'] = round(o['valueQuantity']['value'], 2)
+                observation['unit'] = o['valueQuantity']['unit']
+            if observation['dateSort'] <= datetime_to and date_from == "":
+                observation_list.append(observation)
+            elif observation['dateSort'] >= datetime_from and date_to == "":
+                observation_list.append(observation)
+            elif observation['dateSort'] >= datetime_from and observation['dateSort'] <= datetime_to:
+                observation_list.append(observation)
+            else:
+                continue
+
+        observation_list = sorted(
+            observation_list, key=lambda d: d['dateSort'], reverse=True)
+
+        #TODO get and display medications
+
+        return render_template("patientData.html", patient_data=patient, medical_data=observation_list)
+
 
 if __name__ == "__main__":
-    patient = client.reference('Patient', '31191928-6acb-4d73-931c-e601cc3a13fa')
-    observations = client.resources('Observation').search(subject=patient)
-    print(patient.to_resource()['gender'])
-    app.run(debug=True)
+    patient = client.reference(
+        'Patient', '31191928-6acb-4d73-931c-e601cc3a13fa')
+    statements = client.resources('MedicationRequest').search(subject=patient)
+    for s in statements:
+        print(s['medicationCodeableConcept'])
+    
+    #app.run(debug=True)
